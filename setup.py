494f3ba6b8067f1dcbe4ad106a66d0373eb52abe
Pu Zheng <zhengpuas47@gmail.com>
2021-11-16 14:17:36 -0500

    test setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ChromAn',
+    version='0.1a',
+    packages=['ChromAn'],
+    install_requires=[
+        'requests',
+        'importlib; python_version == "3.8"',
+    ],
+)
