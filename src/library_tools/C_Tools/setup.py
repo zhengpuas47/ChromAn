e8cb6d231baf69f280c4213415369aa2a7716e6f
zhengpuas47 <zhengpuas47@users.noreply.github.com>
2023-03-29 02:58:28 -0400

    fix hard code pathsep

--- src/library_tools/C_Tools/setup.py
+++ src/library_tools/C_Tools/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy as np
-setup(name='Fast oligo tools',ext_modules=cythonize(r".\seqint.pyx"), include_dirs=[np.get_include()])
+import os
+setup(name='Fast oligo tools',ext_modules=cythonize( '.'+os.pathsep+r"seqint.pyx"), include_dirs=[np.get_include()])
 #setup(name='Fast oligo tools',ext_modules=cythonize(r"C_Tools\seqint.pyx"), include_dirs=[np.get_include()])
