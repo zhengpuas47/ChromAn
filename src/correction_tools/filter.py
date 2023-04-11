f5070e90b60fcbf66b23575437cf630784957840
zhengpuas47 <zhengpuas47@gmail.com>
2023-04-10 22:50:36 -0400

    fix import

--- src/correction_tools/filter.py
+++ src/correction_tools/filter.py
@@ -1,4 +1,4 @@
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import gaussian_filter
 import numpy as np
 
 def gaussian_deconvolution(im, gfilt_size=2, niter=1):
@@ -9,7 +9,6 @@ def gaussian_deconvolution(im, gfilt_size=2, niter=1):
         decon_im = decon_im / gaussian_filter(decon_im, gfilt_size)
     
     return decon_im
-
 
 def gaussian_high_pass_filter(image, sigma=5, truncate=2):
     """Apply gaussian high pass filter to given image"""
