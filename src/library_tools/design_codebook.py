503ab94f9e8c89b150862bb8c17c1b03a8c904b3
zhengpuas47 <zhengpuas47@gmail.com>
2023-05-22 22:11:38 -0400

    NTS library design codes

--- src/library_tools/design_codebook.py
+++ src/library_tools/design_codebook.py
@@ -31,6 +31,46 @@ def remove_bad_barcodes(barcodes, min_hamming_dist=4):
         #break
     return _kept_barcodes
 
+def select_balanced_subsets(barcodes, sel_num, ):
+    """Given all possible barcodes with set Hamming distances and barcodes, """
+    # check inputs
+    if len(barcodes) < sel_num:
+        raise ValueError(f"try to select more barcodes than given. ({sel_num} of {len(barcodes)})")
+    print(f"- Selecting {sel_num} barcodes among {len(barcodes)}")
+    _barcodes = np.array(barcodes) # internalize
+    _dist_mat = HammingDist_for_matrix(barcode_to_matrix(_barcodes)) # pre-calculate distance matrix
+    _min_hd = np.unique(_dist_mat)[1]
+    #print(f"Minimum Hamming dist: {_min_hd}")
+    # init and select
+    _sel_inds = [np.random.randint(len(barcodes))] # start with one random
+    _bit_usage = np.zeros(len(np.unique(barcodes)))
+    _bit_usage[_barcodes[_sel_inds[-1]]] += 1
+    while len(_sel_inds) < sel_num:
+        #print(_sel_inds)
+        # select existing
+        _cand_inds = np.setdiff1d(np.arange(len(barcodes)), np.array(_sel_inds))
+        _cand_dist_counts = (_dist_mat[np.array(_sel_inds)][:, _cand_inds] == _min_hd).sum(0)
+        _cand_usages = np.array([ np.sum(_bit_usage[_barcodes[_i]]) for _i in _cand_inds])
+        # for the minimal overlapped bits, select the most balanced one
+        _min_overlap_flags = np.where(_cand_dist_counts == np.min(_cand_dist_counts))[0]
+        _min_overlap_usages = _cand_usages[_min_overlap_flags]
+        _min_usage_flags = np.where(_min_overlap_usages == np.min(_min_overlap_usages))[0]
+        _final_cand_inds = _cand_inds[_min_overlap_flags][_min_usage_flags]
+        # randomly select among final candidates
+        _final_sel_ind = _final_cand_inds[np.random.randint(len(_final_cand_inds))]
+        # append
+        _sel_inds.append(_final_sel_ind)
+        _bit_usage[_barcodes[_sel_inds[-1]]] += 1
+        #print(len(_min_overlap_flags), len(_min_usage_flags))
+        #print(_sel_inds)
+        #break
+    # return final barcodes:
+    _sel_inds = np.sort(_sel_inds)
+    _unused_inds = np.setdiff1d(np.arange(len(barcodes)), np.array(_sel_inds))
+    _sel_barcodes = _barcodes[_sel_inds]
+    _unused_barcodes = _barcodes[_unused_inds]
+    return _sel_barcodes, _unused_barcodes
+
 # Calculation
 def HammingDist_for_matrix(matrix):
     from scipy.spatial.distance import pdist, squareform
@@ -38,3 +78,4 @@ def HammingDist_for_matrix(matrix):
     print(f"minimum hamming distance: {np.min(_dists):1n}")
     _distmat = squareform(_dists)
     return _distmat
+
