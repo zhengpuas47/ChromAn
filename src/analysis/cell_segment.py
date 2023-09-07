ba5581223c0ae2911e6b0b1d7495471452495a2e
zhengpuas47 <zhengpuas47@gmail.com>
2023-09-07 14:12:33 -0400

    add note

--- src/analysis/cell_segment.py
+++ src/analysis/cell_segment.py
@@ -21,4 +21,14 @@ class CellSegmentTask(AnalysisTask):
         print('parameters:', self.analysis_parameters)
         
         # load dapi image from the previous round:
-        
+        # get params
+        _loading_params = self.analysis_parameters.get('loading_params', dict()) # kewargs format
+        _fitting_params = self.analysis_parameters.get('fitting_params', dict()) # kewargs format
+ 
+         # get output from previous step:
+        _save_folder = self.analysis_parameters.get('save_folder', os.path.dirname(self.image_filename))
+        _save_filename = os.path.join(_save_folder,
+            os.path.basename(self.image_filename).replace(os.extsep+self.image_filename.split(os.extsep)[-1], os.extsep+'hdf5'))
+        # load color_usage to determine channels to be processed:
+        color_usage_df = Color_Usage(self.color_usage)
+        hyb_folder = os.path.basename(os.path.dirname(self.image_filename))
